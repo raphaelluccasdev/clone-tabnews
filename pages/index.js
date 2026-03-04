9862ac9c051beb79b8a4080db4ebb158a42b4948
raphaelluccasdev <raphaelluccasdev@gmail.com>
2026-03-04 10:39:13 -0300

    commit, limpando o index

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Beatriz, eu te amo! Se você me ama dá uma risadinha! </h1>;
 }
 
-function teste() {
-  console.log("Teste");
-}
-
-function teste2() {
-  console.log("Teste 2 de identação");
-}
-
 export default Home;
